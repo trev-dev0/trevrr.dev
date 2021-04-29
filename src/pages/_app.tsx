2e794d024fbde6ac10ed820c2b861488db53927b
Trevor Rice <trevor.rice260@gmail.com>
2021-04-29 14:33:11 -0400

    added chakra ui

---
+++ src/pages/_app.tsx
@@ -0,0 +1,23 @@
+import { ChakraProvider } from "@chakra-ui/react";
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <ChakraProvider>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
+}
+
+// Only uncomment this method if you have blocking data requirements for
+// every single page in your application. This disables the ability to
+// perform automatic static optimization, causing every page in your app to
+// be server-side rendered.
+//
+// MyApp.getInitialProps = async (appContext) => {
+//   // calls page's `getInitialProps` and fills `appProps.pageProps`
+//   const appProps = await App.getInitialProps(appContext);
+//
+//   return { ...appProps }
+// }
+
+export default MyApp;
---
+++ src/pages/index.tsx
@@ -0,0 +1,6 @@
+import {Button} from "@chakra-ui/react";
+const Index = () => {
+  return <Button colorScheme="purple" size="md">chakra test</Button>;
+};
+
+export default Index;
