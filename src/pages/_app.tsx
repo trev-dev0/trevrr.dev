a12366c3b12d5e3695174bd097d71407fd0dcc78
Trevor Rice <trevor.rice260@gmail.com>
2021-05-03 18:02:56 -0400

    added custom styles after css reset

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,13 +7,56 @@ import { useRouter } from "next/router";
 import LoadingScreen from "../components/LoadingScreen";
 import { Box } from "@chakra-ui/react";
 import Router from "next/router";
-import {motion} from "framer-motion";
+import { motion } from "framer-motion";
 function MyApp({ Component, pageProps }) {
- const router = useRouter(); 
+  const router = useRouter();
   return (
     <ChakraProvider theme={theme}>
       <CSSReset />
-     <motion.div
+      <style global jsx>{`
+        html,
+        body,
+        body > div:first-child,
+        div#__next,
+        __next,
+        div#__next > div {
+          height: 100%;
+        }
+        #__next {
+          height: 100%;
+        }
+        body {
+          height: 100%;
+        }
+        /* width */
+        ::-webkit-scrollbar {
+          width: 10px;
+        }
+
+        /* Track */
+        ::-webkit-scrollbar-track {
+          background: #f1f1f1;
+          box-shadow: inset 0 0 5px grey;
+          border-radius: 10px;
+        }
+
+        /* Handle */
+        ::-webkit-scrollbar-thumb {
+          background: #888;
+          background: #805ad5;
+          border-radius: 10px;
+        }
+
+        /* Handle on hover */
+        ::-webkit-scrollbar-thumb:hover {
+          background: #555;
+        }
+        .js-focus-visible :focus:not([data-focus-visible-added]) {
+          outline: none;
+          box-shadow: none;
+        }
+      `}</style>
+      <motion.div
         key={router.route}
         style={{
           height: "100%",
@@ -21,7 +64,7 @@ function MyApp({ Component, pageProps }) {
           overflow: "hidden",
           position: "fixed",
           // @ts-ignore
-          zIndex: "55"
+          zIndex: "55",
         }}
         id="loadingWrapper"
         initial="pageInitial"
@@ -41,12 +84,11 @@ function MyApp({ Component, pageProps }) {
           },
         }}
       >
-        <LoadingScreen  />
+        <LoadingScreen />
       </motion.div>
-    <Box h="100%" w="100%" zIndex={0}>
-      <Component {...pageProps} />
-    </Box>
-     
+      <Box h="100%" w="100%" zIndex={0} >
+        <Component {...pageProps} />
+      </Box>
     </ChakraProvider>
   );
 }
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,50 +6,6 @@ export default class Document extends NextDocument {
     return (
       <Html lang="en">
         <Head />
-        <style global jsx>{`
-          html,
-          body,
-          body > div:first-child,
-          div#__next,
-          __next,
-          div#__next > div {
-            height: 100%;
-          }
-          __next {
-            height: 100%;
-          }
-          body {
-            height: 100%;
-          }
-          /* width */
-          ::-webkit-scrollbar {
-            width: 10px;
-          }
-
-          /* Track */
-          ::-webkit-scrollbar-track {
-            background: #f1f1f1;
-            box-shadow: inset 0 0 5px grey;
-            border-radius: 10px;
-          }
-
-          /* Handle */
-          ::-webkit-scrollbar-thumb {
-            background: #888;
-            background: #805ad5;
-            border-radius: 10px;
-          }
-
-          /* Handle on hover */
-          ::-webkit-scrollbar-thumb:hover {
-            background: #555;
-          }
-          .js-focus-visible :focus:not([data-focus-visible-added]) {
-            outline: none;
-            box-shadow: none;
-          }
-        `}</style>
-
         <body>
           <ColorModeScript initialColorMode={"dark"} />
           <Main />
